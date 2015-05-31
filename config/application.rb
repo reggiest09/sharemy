5ebf59bc7c9fb5cfb99f05c978b0d5f0a464c102
Reggie Tucker <reggiest09@gmail.com>
2015-05-31 01:07:09 -0400

    key2

--- config/application.rb
+++ config/application.rb
@@ -24,23 +24,8 @@ module Sharemy
     config.active_record.raise_in_transactional_callbacks = true
   # config/environments/production.rb
 
-config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
-}
-
-
-
 
 
   end
-
-
-
-
 
 end
