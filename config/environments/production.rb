6e5749a9ccf979e05b3852e3bca60e1e10bd995d
Reggie Tucker <reggiest09@gmail.com>
2015-06-02 11:00:40 -0400

    devise and flash messages

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,8 @@ config.paperclip_defaults = {
   }
 }
 
-
+#required for devise. Remember to change localhost to app host
+config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
 
 end
