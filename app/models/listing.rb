5068e2fed0eae3dc183c1ec0cd0503d26b5ef8ab
Reggie Tucker <reggiest09@gmail.com>
2015-06-02 14:53:25 -0400

    Associate listing with users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,4 +5,7 @@ has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }
 validates :name, :description, :price, presence: true
 validates :price, numericality: {greater_than: 0}
 validates_attachment_presence :image
+
+belongs_to :user
+
 end
